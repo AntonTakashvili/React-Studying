ec012e40f1b25381a14a63654a9997f64bc05f11
Anton <44674819+CapitanTeemo@users.noreply.github.com>
2021-04-04 22:48:40 +0400

    Update Counter.js
    Change number in correct order

--- src/Counter.js
+++ src/Counter.js
@@ -6,11 +6,11 @@ const Counter = () => {
         <div>
             <div>{count}</div>
             <button onClick={() => setCount((prev) => prev + 1)}>+1</button>
-            <button onClick={() => setCount((prev) => prev + 5)}>+1</button>
-            <button onClick={() => setCount((prev) => prev + 100)}>+1</button>
-            <button onClick={() => setCount((prev) => prev - 1)}>+1</button>
-            <button onClick={() => setCount((prev) => prev - 5)}>+1</button>
-            <button onClick={() => setCount((prev) => prev - 100)}>+1</button>
+            <button onClick={() => setCount((prev) => prev + 5)}>+5</button>
+            <button onClick={() => setCount((prev) => prev + 100)}>+100</button>
+            <button onClick={() => setCount((prev) => prev - 1)}>-100</button>
+            <button onClick={() => setCount((prev) => prev - 5)}>-5</button>
+            <button onClick={() => setCount((prev) => prev - 100)}>-1</button>
 
         </div>
     )
